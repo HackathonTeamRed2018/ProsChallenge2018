db6ae30481045440b1c8cdf4e17ba72417a4f73a
Michael Panagos <michael@technicool.com>
2018-11-03 23:40:36 -0500

    added get price to express

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,44 @@
 var express = require('express');
 var router = express.Router();
-
+let price = require("../lib/priceGetter");
+let data;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/results', function(req, res, next) {
-  let data = {from: "Houston, TX", to: "Seattle, WA", price: 420, distance: 500, stops: 1};
+
+ if(!data)
+ { 
+    data = {from: "", to: "", price:0 , distance: 0, stops: 0};
+ }
+ console.log(JSON.stringify(data));
   res.render('results', { results: data });
 });
 
 router.post('/results', function(req, res, next) {
-  if (req.body) {
-    for (i in req.body) {
-      console.log(i);
-    }
-  }
-  res.render('results', { title: "Search Results!", results: req.body })
+  console.log(JSON.stringify(req.body));
+  console.log("req.body.fromLocation= "+req.body.fromLocation);
+  console.log("req.body.toLocation= "+req.body.toLocation);
+
+  
+  price.findPrice(req.body.fromLocation , req.body.toLocation )
+        .then((resolve)=>{
+           data = {
+            from: "Houston, TX",
+             to: "Seattle, WA",
+              price:  resolve.price,
+               distance: resolve.duration,
+                stops: 1
+              };
+              res.redirect('/results');
+        })
+        .catch((err)=>{
+            console.log('there was an error= '+err);
+            
+        });
+  // res.render('results', { title: "Search Results!", results: req.body })
 });
 
 module.exports = router;
--- test/app/app.js
+++ test/app/app.js
@@ -3,7 +3,7 @@
 // =================================================================================
 // App Configuration
 // =================================================================================
-const https = require('https');
+var price = require("/Users/micha/Desktop/projects/git/ProsChallenge2018/lib/priceGetter");
 
 const {
     App
@@ -37,7 +37,7 @@ app.setHandler({
         console.log("this is passed in" + JSON.stringify(city));
         // console.log("this is passed in" + JSON.stringify(date));
         // console.log("this is passed in"+JSON.stringify(geoCity2));
-        findPrice(city.value[0] , city.value[1] )
+        price.findPrice(city.value[0] , city.value[1] )
         .then((resolve)=>{
             this.tell('Hey it cost ' + resolve.price + ". duration is  " + resolve.duration + " thats a nice place");
         })
@@ -51,40 +51,6 @@ app.setHandler({
 });
 
 
-function findPrice(from,to) {
-    return new Promise((resolve,reject)=>{
-    let from_loc = from
-    let to_loc = to
 
-    let query = 'https://api.skypicker.com/flights?fly_from=' + from_loc + '&fly_to=' + to_loc + '&limit=5&sort=price&curr=USD';
-
-    https.get(query, (resp) => {
-        let data = '';
-
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            let objdata = JSON.parse(data);
-            let obj={
-                "price":objdata.best_results[0].price,
-                "duration":objdata.best_results[0].duration
-            };
-
-            resolve(obj);
-            console.log(objdata.best_results[0].price);
-            console.log(objdata.best_results[0].duration);
-        });
-
-    }).on("error", (err) => {
-        reject(err);
-        console.log("Error: " + err.message);
-    });
-});
-
-}
 
 module.exports.app = app;
