c1e28c5ff508538470ac5789109557b59d422352
Oscar A. Tovar <oscar.alberto.tovar@gmail.com>
2018-11-03 21:19:10 -0500

    Updated views in Express. Routes updated.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/results', function(req, res, next) {
+  let data = {from: "Houston, TX", to: "Seattle, WA", price: 420, distance: 500, stops: 1};
+  res.render('results', { results: data });
+});
+
+router.post('/results', function(req, res, next) {
+  if (req.body) {
+    for (i in req.body) {
+      console.log(i);
+    }
+  }
+  res.render('results', { title: "Search Results!", results: req.body })
+});
+
 module.exports = router;
